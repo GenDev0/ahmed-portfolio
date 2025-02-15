4d0fc445ebf3524d8e2e2dda4d8c87c5025fa9b8
GenDev0 <dev.Gen.info0@gmail.com>
2025-02-16 00:23:22 +0100

    feat(grid): start Grid section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/floating-nav";
+import { navItems } from "@/data";
 
 export default function Home() {
   return (
-    <main className="bg-black-100 relative mx-auto flex flex-col items-center justify-center overflow-hidden px-5 sm:px-10">
+    <main className="relative mx-auto flex flex-col items-center justify-center overflow-hidden bg-black-100 px-5 sm:px-10">
       <div className="w-full max-w-7xl">
+        <FloatingNav navItems={navItems} />
         <Hero />
+        <Grid />
       </div>
     </main>
   );
