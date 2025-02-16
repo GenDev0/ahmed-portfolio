90bf3088526f26d1f646a8d007baaf60834ec96e
GenDev0 <dev.Gen.info0@gmail.com>
2025-02-16 10:33:10 +0100

    fix(app): change buttons to use client

--- components/ui/spotlight.tsx
+++ components/ui/spotlight.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { cn } from "@/lib/utils";
 
 type OldSpotlightProps = {
@@ -9,7 +11,7 @@ export const OldSpotlight = ({ className, fill }: OldSpotlightProps) => {
   return (
     <svg
       className={cn(
-        "animate-spotlight pointer-events-none absolute z-[1] h-[169%] w-[138%] opacity-0 lg:w-[84%]",
+        "pointer-events-none absolute z-[1] h-[169%] w-[138%] animate-spotlight opacity-0 lg:w-[84%]",
         className
       )}
       xmlns="http://www.w3.org/2000/svg"
